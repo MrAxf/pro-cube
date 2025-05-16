26c6848c53091e38d74cafff722a58a258907bb0
Juan Andres <theaxford@gmail.com>
2025-05-16 16:27:12 +0200

    Update version to 0.7.0 in package and jsr configuration

--- app/pages/dashboard.vue
+++ app/pages/dashboard.vue
@@ -1,14 +1,6 @@
 <template>
   <div class="h-[calc(100dvh-3.75rem)] w-full">
     <CubePlayground />
-    <!-- <CubeIcon
-      class="size-[400px]"
-      :size="4"
-      mode="rotation"
-      :layer="3"
-      axis="Y"
-      rotate="backward"
-    /> -->
   </div>
 </template>
 
